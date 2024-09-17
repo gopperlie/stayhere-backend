f2ca96cc7a1951a721ec3af8b34751faf22d129f
MH S <gopperlie123@gmail.com>
2024-09-17 23:31:12 +0800

    router properties

--- backend/controllers/PropertiesController.js
+++ backend/controllers/PropertiesController.js
@@ -1,5 +1,15 @@
-const { Router } = require("express");
-
+const express = require("express");
+const pool = require("../persistence/db");
 const router = express.Router();
 
-Router;
+router.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM properties");
+    res.json(result.rows); // Send the fetched rows as JSON
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const pool = require("./persistence/db");
 const getGreeting = require("./routes/getGreeting");
+const propertyRouter = require("./controllers/PropertiesController");
 // const bodyParser = require("body-parser");
 
 const app = express();
@@ -17,15 +18,7 @@ app.use(
   })
 );
 
-app.get("/properties", async (req, res) => {
-  try {
-    const result = await pool.query("SELECT * FROM properties");
-    res.json(result.rows); // Send the fetched rows as JSON
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server Error");
-  }
-});
+app.use("/api/properties", propertyRouter);
 
 app.get("/", (request, response) => {
   response.json({ info: "Node.js, Express, and Postgres API" });
