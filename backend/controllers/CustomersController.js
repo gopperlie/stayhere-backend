afc5a32fe4f8f07dc2832c43c0bb604389a9c5af
MH S <gopperlie123@gmail.com>
2024-09-20 00:05:45 +0800

    coded some cx routes

---
+++ backend/controllers/CustomersController.js
@@ -0,0 +1,104 @@
+const express = require("express");
+const pool = require("../persistence/db");
+const router = express.Router();
+const debug = require("debug")("controller:customers");
+
+// POST route to add new data
+router.post("/add", async (req, res) => {
+  const {
+    family_name,
+    given_name,
+    email,
+    phone_number,
+    nationality,
+    date_of_birth,
+    gender,
+  } = req.body;
+
+  if (!family_name || !given_name || !email) {
+    return res.status(400).json({ error: "Name and email are required" });
+  }
+
+  try {
+    const query =
+      "INSERT INTO customers (family_name, given_name, email, phone_number, nationality, date_of_birth, gender) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *";
+    const values = [
+      family_name,
+      given_name,
+      email,
+      phone_number,
+      nationality,
+      date_of_birth,
+      gender,
+    ];
+    const result = await pool.query(query, values);
+
+    res.status(201).json({
+      message: "Customer added successfully",
+      customer: result.rows[0],
+    });
+  } catch (error) {
+    console.error("Error inserting data:", error);
+    res.status(500).json({ error: "Database error" });
+  }
+});
+
+router.delete("/remove/:id", async (req, res) => {
+  const cxid = parseInt(req.params.id);
+
+  if (isNaN(cxid)) {
+    return res.status(400).json({ error: "Invalid customer ID" });
+  }
+
+  try {
+    // First, check if the customer exists
+    const checkQuery = "SELECT * FROM customers WHERE customer_id = $1";
+    const checkResult = await pool.query(checkQuery, [cxid]);
+
+    if (checkResult.rowCount === 0) {
+      // If no rows were found, the customer does not exist
+      return res.status(404).json({ error: "Customer not found" });
+    }
+
+    // Customer exists, proceed to delete
+    const deleteQuery = "DELETE FROM customers WHERE customer_id = $1";
+    await pool.query(deleteQuery, [cxid]);
+
+    res.status(200).send(`Customer deleted with ID: ${cxid}`);
+  } catch (error) {
+    console.error("Unable to delete customer:", error);
+    res.status(500).json({ error: "Database error" });
+  }
+});
+
+//nned to handle instances where certain fields are left out, no change should be made
+router.put("/update/:id", async (req, res) => {
+  const cxid = parseInt(req.params.id);
+
+  if (isNaN(cxid)) {
+    return res.status(400).json({ error: "Invalid customer ID" });
+  }
+
+  try {
+    const checkQuery = "SELECT * FROM customers WHERE customer_id = $1";
+    const checkResult = await pool.query(checkQuery, [cxid]);
+
+    if (checkResult.rowCount === 0) {
+      // If no rows were found, the customer does not exist
+      return res.status(404).json({ error: "Customer not found" });
+    }
+    const { gender, email } = req.body;
+    const updateQuery =
+      "UPDATE customers SET gender = $1, email = $2 WHERE customer_id = $3 RETURNING *";
+    const result = await pool.query(updateQuery, [gender, email, cxid]);
+    res.status(200).json({
+      message: `Customer ${cxid} updated successfully`,
+      customer: result.rows[0],
+    });
+  } catch (error) {
+    console.error("Unable to update customer:", error);
+    res.status(500).json({ error: "Database error" });
+  }
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const pool = require("./persistence/db");
+const debug = require("debug")("app:server");
 const getGreeting = require("./routes/getGreeting");
 const propertyRouter = require("./controllers/PropertiesController");
+const customerRouter = require("./controllers/CustomersController");
 // const bodyParser = require("body-parser");
 
 const app = express();
@@ -19,6 +21,7 @@ app.use(
 );
 
 app.use("/api/properties", propertyRouter);
+app.use("/api/customers", customerRouter);
 
 app.get("/", (request, response) => {
   response.json({ info: "Node.js, Express, and Postgres API" });
@@ -27,5 +30,5 @@ app.get("/", (request, response) => {
 app.get("/api/greeting", getGreeting);
 
 app.listen(port, () => {
-  console.log(`linproperties listening on port ${port}`);
+  debug(`stayhere listening on port ${port}`);
 });
